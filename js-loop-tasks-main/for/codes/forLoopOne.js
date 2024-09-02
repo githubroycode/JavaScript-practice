b7897365676480249f46954c440c2128417053f3
BadhonRoy <mrbadhonroy@hotmail.com>
2024-09-02 07:34:18 +0600

    added loop and conditional

---
+++ js-loop-tasks-main/for/codes/forLoopOne.js
@@ -0,0 +1,4 @@
+
+for(let target = 1; target<=60; target++){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
---
+++ js-loop-tasks-main/while/codes/whileLoop.js
@@ -0,0 +1,11 @@
+// Even Number
+
+let num = 1;
+let sum = 0;
+while(num <= 10){
+    console.log(num);
+   if(num % 2 === 0){
+    console.log("Even number: "+num);
+   }
+    num++;
+}
