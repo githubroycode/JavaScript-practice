734116b72bd399d0c9b2b46bc564b2ca46d9f4c4
BadhonRoy <mrbadhonroy@hotmail.com>
2024-09-03 11:27:02 +0600

    updated

---
+++ js-loop-tasks-main/for/codes/evenForLoopTwo.js
@@ -0,0 +1,6 @@
+for(i=61; i<=100; i++){
+    console.log(i);
+    if(i%2===0){
+        console.log('This is even number: ',i);
+    }
+}
---
+++ js-loop-tasks-main/for/codes/sumOddNumbersForThree.js
@@ -0,0 +1,5 @@
+let sum = 0;
+for (let i = 91; i <= 129; i = i + 1) {
+    sum = sum + i;
+    console.log(sum);
+}
